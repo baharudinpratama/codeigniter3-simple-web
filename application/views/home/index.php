3383e1acd6aadf22baf73d1058c2fabe9a54b469
Baharudin Pratama <baharudinp21@gmail.com>
2022-03-14 12:35:33 +0700

    setup home page

---
+++ application/views/home/index.php
@@ -0,0 +1,9 @@
+<div class="container">
+    <div class="jumbotron">
+        <h1 class="display-4">Hello, <?= $name; ?>!</h1>
+        <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, dolorum.</p>
+        <hr class="my-4">
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, reprehenderit impedit minus ea dignissimos veritatis! Illum excepturi quasi perspiciatis voluptate, sit cumque blanditiis voluptatem sequi.</p>
+        <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+    </div>
+</div>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -9,7 +9,22 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title><?= $title; ?></title>
 </head>
 
 <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light mb-3">
+        <div class="container">
+            <a class="navbar-brand" href="#">CI App</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="#">Students</a>
+                    <a class="nav-link" href="#">Profile</a>
+                </div>
+            </div>
+        </div>
+    </nav>
