f609b9759e547816317e5565d98aa0c8739070dc
Baharudin Pratama <baharudinp21@gmail.com>
2022-03-14 16:20:01 +0700

    add custom css link to header

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -9,6 +9,9 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
 
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="<?= base_url('assets/css/style.css'); ?>">
+
     <title><?= $title; ?></title>
 </head>
 
